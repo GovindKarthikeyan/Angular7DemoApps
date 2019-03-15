66a71f0844d05a9476d22f0580d7d6a7945db3dd
LUXINTERACTIVE\KGovindarasu <Karthikeyan.Govindarasu@LuxInteractive.com>
2019-03-14 21:21:54 -0400

    Ngrx schematics commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './store/effects/app.effects';
 
 @NgModule({
   declarations: [
@@ -9,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forFeature([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
