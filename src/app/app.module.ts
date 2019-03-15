8a16252f0c231fb42e79ebb1274b4ee5a0f80998
LUXINTERACTIVE\KGovindarasu <Karthikeyan.Govindarasu@LuxInteractive.com>
2019-03-14 22:10:08 -0400

    Core mule creation and Store Restructure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store/reducers';
+import { reducers, metaReducers } from './core/store/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './store/effects/app.effects';
+import { AppEffects } from './core/store/effects/app.effects';
 
 @NgModule({
   declarations: [
