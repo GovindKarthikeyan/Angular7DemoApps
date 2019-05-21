27d0211024c6fbfa8ae9e6483ac4f1f66064484f
LUXINTERACTIVE\KGovindarasu <Karthikeyan.Govindarasu@LuxInteractive.com>
2019-05-21 17:23:13 -0400

    Custom Observable creation demo.

--- src/app/features/check-value-changes/check-value-changes.component.ts
+++ src/app/features/check-value-changes/check-value-changes.component.ts
@@ -1,16 +1,28 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Observable, Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, of, from, interval } from 'rxjs';
+import { tap, delay } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-check-value-changes',
 	templateUrl: './check-value-changes.component.html',
 	styleUrls: ['./check-value-changes.component.scss']
 })
-export class CheckValueChangesComponent implements AfterViewInit {
+export class CheckValueChangesComponent implements OnInit, AfterViewInit {
 	@ViewChild('form') form: NgForm;
 	valueChanges$: Observable<any>;
+	timer$: Observable<number>;
+
+	ngOnInit() {
+		this.timer$ = interval(1000); //from([1, 2, 3, 4, 5]);
+		this.timer$.pipe(
+			delay(1000),
+		).subscribe(
+			(n) => console.log(n),
+			null,
+			() => console.log('finished...')
+		);
+	}
 
 	ngAfterViewInit() {
 		this.valueChanges$ = this.form.valueChanges.pipe(
